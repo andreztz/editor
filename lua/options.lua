64adf6706d53d7c1d0c66ee79bd0459d8693f9be
André P. Santos <andreztz@gmail.com>
2023-07-26 14:35:47 -0300

    Corrige erros de digitação

--- lua/options.lua
+++ lua/options.lua
@@ -1,7 +1,7 @@
 -- Mapeamento de teclas
 local opt = vim.opt
 
--- Sincroniza com a area de tranferencia do sistema
+-- Sincroniza com a área de tranferência do sistema
 opt.clipboard = "unnamedplus"
 -- Habilita mouse
 opt.mouse = "a"
@@ -13,11 +13,11 @@ opt.termguicolors = true
 opt.cursorline = true
 
 -- Indentação
--- Converte espaços em tabulações
+-- Converte espaços em tabulação
 opt.expandtab = true
 -- Define o número de espaços inseridos para cada indentação
 opt.shiftwidth = 4
--- Insere indentação automaticamente
+-- Insere indentação automáticamente
 opt.smartindent = true
 -- Número de espaços para cada tabulação
 opt.tabstop = 4
