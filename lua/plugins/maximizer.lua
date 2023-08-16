eb369fdf2991b49398215043a81f8d30921b2c13
André P. Santos <andreztz@gmail.com>
2023-08-16 06:04:50 -0300

    Adiciona plugin `maximazer`

---
+++ lua/plugins/maximizer.lua
@@ -0,0 +1,12 @@
+-- https://github.com/0x00-ketsu/maximizer.nvim
+return {
+    "andreztz/maximizer.nvim",
+    keys = {
+        {
+            "<A-\\>",
+            "<cmd>lua require('maximizer').toggle()<cr>",
+            mode = { "n" },
+            desc = ""
+        },
+    }
+}
