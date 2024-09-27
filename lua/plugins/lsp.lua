83b17b8de8934707b408ab17b362d51efeab62c7
André P. Santos <andreztz@gmail.com>
2024-09-27 00:46:21 -0300

    Migrate lsp-zero from v3 to v4

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,27 +1,152 @@
-return {
-    "VonHeikemen/lsp-zero.nvim",
-    branch = "v3.x",
-    dependencies = {
-        { "neovim/nvim-lspconfig" },
-        {
-            "williamboman/mason.nvim",
-            -- build = function()
-            --     pcall(vim.cmd, 'MasonUpdate')
-            -- end,
-        },
-        { "williamboman/mason-lspconfig.nvim" },
-        { "hrsh7th/nvim-cmp" },
-        { "hrsh7th/cmp-nvim-lsp" },
-        { "L3MON4D3/LuaSnip" },
-        { "jose-elias-alvarez/null-ls.nvim" },
-    },
-    config = function()
-        local lsp = require("lsp-zero").preset("recommended")
+local _ensure_installed = {
+    "rust_analyzer",
+    "lua_ls",
+    "gopls",
+    "pyright",
+    "ruff",
+    "yamlls", -- yaml,yaml.github yaml.gitlab,yaml.docker-compose
+    "ansiblels",
+    "dockerls",
+    "fish_lsp",
+    "htmx",
+    "zk",
+    "markdown_oxide",
+}
 
-        lsp.on_attach(function(client, bufnr)
-            -- see :help lsp-zero-keybindings
-            -- to learn the available actions
-            lsp.default_keymaps({ buffer = bufnr })
+return {
+    {
+        "VonHeikemen/lsp-zero.nvim",
+        branch = "v4.x",
+        lazy = true,
+        config = false,
+    },
+    {
+        "williamboman/mason.nvim",
+        lazy = false,
+        config = true,
+    },
+    -- Autocompletion
+    {
+        "hrsh7th/nvim-cmp",
+        event = "InsertEnter",
+        dependencies = {
+            { "L3MON4D3/LuaSnip" },
+        },
+        config = function()
+            local cmp = require("cmp")
+            local lsp = require("lsp-zero")
+            local cmp_format = lsp.cmp_format({ details = true })
+            local cmp_action = require("lsp-zero").cmp_action()
+            cmp.setup({
+                sources = {
+                    { name = "nvim_lsp" },
+                },
+                formatting = cmp_format,
+                window = {
+                    completion = cmp.config.window.bordered(),
+                    documentation = cmp.config.window.bordered(),
+                },
+
+                mapping = {
+                    -- `Enter` key to confirm completion
+                    ["<CR>"] = cmp.mapping.confirm({ select = false }),
+                    ["<Tab>"] = cmp_action.luasnip_supertab(),
+                    ["<S-Tab>"] = cmp_action.luasnip_shift_supertab(),
+                    -- Ctrl+Space to trigger completion menu
+                    ["<C-Space>"] = cmp.mapping.complete(),
+                    -- Navigate between snippet placeholder
+                    ["<C-f>"] = cmp_action.luasnip_jump_forward(),
+                    ["<C-b>"] = cmp_action.luasnip_jump_backward(),
+                    -- Scroll up and down in the completion documentation
+                    ["<C-u>"] = cmp.mapping.scroll_docs(-4),
+                    ["<C-d>"] = cmp.mapping.scroll_docs(4),
+                },
+                snippet = {
+                    expand = function(args)
+                        require("luasnip").lsp_expand(args.body)
+                    end,
+                },
+            })
+        end,
+    },
+    -- LSP
+    {
+        "neovim/nvim-lspconfig",
+        cmd = { "LspInfo", "LspInstall", "LspStart" },
+        event = { "BufReadPre", "BufNewFile" },
+        dependencies = {
+            { "hrsh7th/cmp-nvim-lsp" },
+            { "williamboman/mason.nvim" },
+            { "williamboman/mason-lspconfig.nvim" },
+        },
+        config = function()
+            local lsp = require("lsp-zero")
+            -- lsp_attach is where you enable features that only work
+            -- if there is a language server active in the file
+            local lsp_attach = function(client, bufnr)
+                local opts = { buffer = bufnr }
+
+                vim.keymap.set(
+                    "n",
+                    "K",
+                    "<cmd>lua vim.lsp.buf.hover()<cr>",
+                    opts
+                )
+                vim.keymap.set(
+                    "n",
+                    "gd",
+                    "<cmd>lua vim.lsp.buf.definition()<cr>",
+                    opts
+                )
+                vim.keymap.set(
+                    "n",
+                    "gD",
+                    "<cmd>lua vim.lsp.buf.declaration()<cr>",
+                    opts
+                )
+                vim.keymap.set(
+                    "n",
+                    "gi",
+                    "<cmd>lua vim.lsp.buf.implementation()<cr>",
+                    opts
+                )
+                vim.keymap.set(
+                    "n",
+                    "go",
+                    "<cmd>lua vim.lsp.buf.type_definition()<cr>",
+                    opts
+                )
+                vim.keymap.set(
+                    "n",
+                    "gr",
+                    "<cmd>lua vim.lsp.buf.references()<cr>",
+                    opts
+                )
+                vim.keymap.set(
+                    "n",
+                    "gs",
+                    "<cmd>lua vim.lsp.buf.signature_help()<cr>",
+                    opts
+                )
+                vim.keymap.set(
+                    "n",
+                    "<F2>",
+                    "<cmd>lua vim.lsp.buf.rename()<cr>",
+                    opts
+                )
+                vim.keymap.set(
+                    { "n", "x" },
+                    "<F3>",
+                    "<cmd>lua vim.lsp.buf.format({async = true})<cr>",
+                    opts
+                )
+                vim.keymap.set(
+                    "n",
+                    "<F4>",
+                    "<cmd>lua vim.lsp.buf.code_action()<cr>",
+                    opts
+                )
+            end
             -- -- -- check file type, if Go then use GoFmt else the default
             -- if vim.bo.filetype == 'go' then
             --     vim.keymap.set(
@@ -32,97 +157,43 @@ return {
             --         '<cmd>lua vim.lsp.buf.format({async = true})<cr>',
             --         opts)
             -- end
-        end)
+            lsp.extend_lspconfig({
+                sign_text = true,
+                lsp_attach = lsp_attach,
+                capabilities = require("cmp_nvim_lsp").default_capabilities(),
+            })
 
-        lsp.set_preferences({
-            suggest_lsp_servers = false,
-            sign_icons = {
-                error = "E",
-                warn = "W",
-                hint = "H",
-                info = "I",
-            },
-        })
-        require("mason").setup({})
-        -- go to https://github.com/williamboman/mason-lspconfig.nvim
-        -- to see list of languages
-        require("mason-lspconfig").setup({
-            ensure_installed = {
-                "rust_analyzer",
-                "tsserver",
-                "lua_ls",
-                "gopls",
-                "pyright",
-                "yamlls", -- yaml,yaml.github yaml.gitlab,yaml.docker-compose
-                "ansiblels",
-                "dockerls",
-                -- "docker_compose_language_service",
-                "htmx",
-                "zk",
-                "markdown_oxide",
-            },
-            handlers = {
-                function(server_name)
-                    require("lspconfig")[server_name].setup({})
-                end,
-                -- -- See `:h mason-lspconfig.setup_handlers()` to
-                -- overrides for specific servers.
-            },
-        })
+            vim.diagnostic.config({
+                virtual_text = true,
+            })
 
-        require("lspconfig").lua_ls.setup(lsp.nvim_lua_ls())
-
-        lsp.setup()
-
-        vim.diagnostic.config({
-            virtual_text = true,
-        })
-
-        local cmp = require("cmp")
-        local cmp_format = lsp.cmp_format({ details = true })
-        local cmp_action = require("lsp-zero").cmp_action()
-
-        cmp.setup({
-            formatting = cmp_format,
-            window = {
-                completion = cmp.config.window.bordered(),
-                documentation = cmp.config.window.bordered(),
-            },
-            -- mapping = cmp.mapping.preset.insert({
-            --   -- scroll up and down the documentation window
-            --   ['<Tab>'] = cmp_action.luasnip_supertab(),
-            --   ['<S-Tab>'] = cmp_action.luasnip_shift_supertab(),
-            --   ['<C-u>'] = cmp.mapping.scroll_docs(-4),
-            --   ['<C-d>'] = cmp.mapping.scroll_docs(4),
-            -- }),
-
-            mapping = {
-                -- `Enter` key to confirm completion
-                ["<CR>"] = cmp.mapping.confirm({ select = false }),
-                ["<Tab>"] = cmp_action.luasnip_supertab(),
-                ["<S-Tab>"] = cmp_action.luasnip_shift_supertab(),
-                -- Ctrl+Space to trigger completion menu
-                ["<C-Space>"] = cmp.mapping.complete(),
-                -- Navigate between snippet placeholder
-                ["<C-f>"] = cmp_action.luasnip_jump_forward(),
-                ["<C-b>"] = cmp_action.luasnip_jump_backward(),
-                -- Scroll up and down in the completion documentation
-                ["<C-u>"] = cmp.mapping.scroll_docs(-4),
-                ["<C-d>"] = cmp.mapping.scroll_docs(4),
-            },
-            snippet = {
-                expand = function(args)
-                    require("luasnip").lsp_expand(args.body)
-                end,
-            },
-        })
-
-        -- local null_ls = require("null-ls")
-        --
-        -- null_ls.setup({
-        --     sources = {
-        --         null_ls.builtins.formatting.blue,
-        --     }
-        -- })
-    end,
+            require("mason-lspconfig").setup({
+                ensure_installed = _ensure_installed,
+                handlers = {
+                    function(server_name)
+                        require("lspconfig")[server_name].setup({})
+                    end,
+                    -- -- See `:h mason-lspconfig.setup_handlers()` to
+                    -- overrides for specific servers.
+                    ["pyright"] = function()
+                        local lspconfig = require("lspconfig")
+                        lspconfig.pyright.setup({
+                            settings = {
+                                pyright = {
+                                    disableOrganizeImports = true,
+                                },
+                                python = {
+                                    analysis = {
+                                        useLibraryCodeForTypes = false,
+                                        -- Desabilita pyright linter
+                                        typeCheckingMode = "basic",
+                                    },
+                                },
+                            },
+                        })
+                    end,
+                },
+            })
+        end,
+    },
 }
